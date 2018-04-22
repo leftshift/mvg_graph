ec12adc516b0f201ab70a147a57b07e50eb12b61
Adrian Schneider <ardy@ardy.io>
2018-04-23 04:03:14 +1000

    filter actual routes, get stations

--- make_graph.go
+++ make_graph.go
@@ -1,6 +1,9 @@
 package main
-import "fmt"
-import "github.com/serjvanilla/go-overpass"
+import (
+    "fmt"
+    "log"
+    "github.com/serjvanilla/go-overpass"
+)
 
 var query string = `
 [out:json];
@@ -23,9 +26,22 @@ foreach(
 
 func main(){
     client := overpass.New()
-    result, _ := client.Query(query)
+    result, ok := client.Query(query)
+    if ok != nil {
+        log.Fatal(ok)
+    } 
     //fmt.Println(result)
-    for k, v := range result.Relations {
-        fmt.Printf("key:[%s], ref:[%s]\n", k, v.Meta.Tags["ref"])
+    for key, v := range result.Relations {
+        if v.Meta.Tags["ref"] == "" {
+            delete(result.Relations, key)
+            continue
+        }
+
+        fmt.Printf("key:[%v], ref:[%s], rel:[%s]\n", v.Meta.ID, v.Meta.Tags["ref"], v.Meta.Tags["type"])
+        for k, m := range v.Members {
+            if m.Role == "stop" {
+                fmt.Printf("\t%v: %v\n", k, m.Node.Meta.Tags["name"])
+            }
+        }
     }
 }
